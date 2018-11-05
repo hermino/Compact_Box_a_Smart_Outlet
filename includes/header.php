30d8615c8c405624cdf541c45729c3f1a2b11d36
hermino <herminojunior@gmail.com>
2018-11-05 11:01:37 -0400

    Projeto

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="./estilo/css/bootstrap.css"s>
+        <link rel="stylesheet" href="">
+        <script src="./estilo/js/jquery.min.js"></script>
+        <script src="../estilo/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row" id="barra">
+                    <ul class="nav">                        
+                        <li class="nav-item">
+                        <a class="nav-link active" href="cadastro.php">CADASTRO</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="selecao.php">SELECIONAR</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
